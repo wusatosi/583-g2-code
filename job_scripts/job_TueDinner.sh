f6023687c9be40c0afa999a4097e02a840217472
Brad Wu <bradwu@umich.edu>
2023-12-13 15:43:41 -0500

    cleanup

--- job_scripts/job_TueDinner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/jespark/taffo/TAFFO/python_script
-
-rm /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a.csv
-cp /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a_default.csv /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a.csv
-python optimize.py --iterations=30 --num_components_min=1 --num_components_max=20 --range_low=0.666666 --range_high=1.500000 --prob_jump=0.5 --no_jump_threshold=1.0 --seed=1
