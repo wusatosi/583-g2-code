ce4cd425338df8f60a3cc5a1e5c952534801579a
Brad Wu <bradwu@umich.edu>
2023-12-13 15:55:00 -0500

    update_script

--- job_scripts/run_ml.sh
+++ job_scripts/run_ml.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd /home/jespark/taffo/TAFFO/python_script
+cd /home/$UNIQNAME/taffo/TAFFO/test/polybench-cpu/checkpoints/$PATH_TO_CKPT
 
-rm /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a.csv
-cp /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a_default.csv /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a.csv
-python optimize.py --iterations=20 --num_components_min=1 --num_components_max=20 --range_low=0.666666 --range_high=1.500000 --prob_jump=0.5 --no_jump_threshold=1.0 --seed=1
-
-mv /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a.csv /home/jespark/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a_run1.csv
+cp /home/$UNIQNAME/taffo/TAFFO/tool/taffo/ILP/cost/eecs583a_default.csv /home/$UNIQNAME/taffo/TAFFO/test/polybench-cpu/checkpoints/$PATH_TO_CKPT
+python combine_csv.py
+python ml_model.py
